e9237dd550b02c29a318257a14f9c2d41129d107
Raul Melo <raulvitor123456@hotmail.com>
2025-03-14 02:59:29 -0300

    setup inicial

---
+++ app/src/main/java/br/com/board/persistence/config/ConnectionConfig.java
@@ -0,0 +1,22 @@
+package br.com.board.persistence.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static lombok.AccessLevel.PRIVATE;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = PRIVATE)
+public final class ConnectionConfig {
+
+    public static Connection getConnection() throws SQLException {
+        var url = "jdbc:mysql://localhost/board_dio";
+        var user = "root";
+        var password = "root";
+        var connection = DriverManager.getConnection(url, user, password);
+        connection.setAutoCommit(false);
+        return connection;
+    }
+
+}
